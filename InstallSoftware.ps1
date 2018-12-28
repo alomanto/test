eb154a56ea85afd7302e540e87255fc0d40a1832
alomanto <alomanto@firstlight.net>
2018-12-28 13:15:29 -0500

    Add files via upload

---
+++ InstallSoftware.ps1
@@ -0,0 +1,15 @@
+﻿If(Test-Path -Path "$env:ProgramData\Chocolatey") {
+    $Packages = 'googlechrome', 'notepadplusplus', 'adobereader', 'firefox', '7zip', 'sysinternals', 'microsoft-teams.install'
+ForEach ($PackageName in $Packages)
+    {
+    choco install $PackageName -y
+    }
+}
+Else {
+Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+$Packages = 'googlechrome', 'notepadplusplus', 'adobereader', 'firefox', '7zip', 'sysinternals', 'microsoft-teams.install'
+ForEach ($PackageName in $Packages)
+    {
+        choco install $PackageName -y
+    }
+}
